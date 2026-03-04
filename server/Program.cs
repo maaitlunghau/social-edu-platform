c50ef2d80d4132eed973cb8d4bcca1f908f2e968
maaitlunghau <chunhau.py@gmail.com>
2026-03-04 17:08:24 +0700

    feat: configure JWT authentication middleware

--- server/Program.cs
+++ server/Program.cs
@@ -1,5 +1,9 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using server.Data;
 using server.Filters;
 using server.Repositories;
@@ -35,6 +39,53 @@ builder.Services.AddScoped<IUserRepository, UserService>();
 builder.Services.AddScoped<IRefreshTokenRepository, RefreshTokenService>();
 builder.Services.AddSingleton<TokenService>();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var secretKey = builder.Configuration["JWT:Key"]
+                    ?? throw new InvalidOperationException("JWT:Key is not configured");
+        var issuer = builder.Configuration["JWT:Issuer"]
+            ?? throw new InvalidOperationException("JWT:Issuer is not configured");
+        var audience = builder.Configuration["JWT:Audience"]
+            ?? throw new InvalidOperationException("JWT:Audience is not configured");
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = issuer,
+            ValidAudience = audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+            ClockSkew = TimeSpan.Zero
+        };
+
+        options.Events = new JwtBearerEvents
+        {
+            OnTokenValidated = async context =>
+            {
+                var jti = context.Principal?.FindFirst(JwtRegisteredClaimNames.Jti)?.Value;
+                if (string.IsNullOrEmpty(jti))
+                {
+                    context.Fail("Missing JTI claim in token");
+                    return;
+                }
+                var db = context.HttpContext.RequestServices.GetRequiredService<DataContext>();
+
+                var refreshToken = await db.RefreshTokenRecords.FirstOrDefaultAsync(
+                    rft => rft.AccessTokenJTI == jti
+                );
+                if (refreshToken != null && !refreshToken.IsActive)
+                {
+                    context.Fail("Token has revoked or expired!");
+                    return;
+                }
+            }
+        };
+    }
+);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -45,6 +96,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 app.MapGet("/", () => Results.Redirect("/swagger"));
 
